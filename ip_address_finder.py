e0acdfb978ed5e47895891d8eca7483e7cde8140
Umut Sağlam <96023206+umutsaglam@users.noreply.github.com>
2024-05-08 04:21:21 +0000

    Add files via upload

---
+++ ip_address_finder.py
@@ -0,0 +1,14 @@
+import socket
+
+def find_ip_address(domain):
+    try:
+        ip_address = socket.gethostbyname(domain)
+        return ip_address
+    except socket.gaierror:
+        return "IP adresi bulunamadı."
+
+if __name__ == "__main__":
+    domain_name = input("Alan adını girin: ")
+    ip_address = find_ip_address(domain_name)
+    print("Alan adı:", domain_name)
+    print("IP adresi:", ip_address)
